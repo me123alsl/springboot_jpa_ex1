0d5acbb25b638d76c9401154d7c16d5c32d8423b
JinseokSong <me123alsl@gmail.com>
2022-12-13 01:49:40 +0900

    20221213 JPA 슈퍼클래스, 인헤리턴스

---
+++ src/main/java/jpashop/JpaMain.java
@@ -0,0 +1,35 @@
+package jpashop;
+
+import jpashop.domain.Movie;
+import jpashop.domain.Order;
+import jpashop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpashop");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+
+            Movie movie = new Movie();
+            movie.setDirector("aaa");
+            movie.setActor("bbb");
+            movie.setName("바람과함께사라지다");
+            movie.setPrice(10000);
+            em.persist(movie);
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
